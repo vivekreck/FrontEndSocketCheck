a09193b7e1c99fd94f710762c3d05aea699e7842
vivek <vivek.reck@gmail.com>
2021-06-26 00:49:57 +0530

    debug

--- Index.js
+++ Index.js
@@ -1,6 +1,6 @@
 import openSocket from 'socket.io-client';
 
-const socket = openSocket('http://localhost:8080');
+const socket = openSocket('https://boiling-river-08041.herokuapp.com/');
 socket.on('color', data => {
     if (data.action === 'change') {
         document.body.style.backgroundColor = data.color;
@@ -11,7 +11,7 @@ const red = document.getElementById('red');
 const yellow = document.getElementById('yellow');
 
 const reqServer = (color) => {
-    fetch('http://localhost:8080/', {
+    fetch('https://boiling-river-08041.herokuapp.com/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ color })
@@ -31,7 +31,7 @@ const reqServer = (color) => {
 red.addEventListener('click', (e) => reqServer('red'))
 yellow.addEventListener('click', (e) => reqServer('yellow'))
 
-fetch('http://localhost:8080/')
+fetch('https://boiling-river-08041.herokuapp.com/')
     .then(resData => {
         return resData.json()
     })
