fef47511baf86e295d75f2665270f1a3bd553625
vivek <vivek.reck@gmail.com>
2021-06-26 00:26:15 +0530

    init

---
+++ Index.js
@@ -0,0 +1,44 @@
+import openSocket from 'socket.io-client';
+
+const socket = openSocket('http://localhost:8080');
+socket.on('color', data => {
+    if (data.action === 'change') {
+        document.body.style.backgroundColor = data.color;
+    }
+})
+
+const red = document.getElementById('red');
+const yellow = document.getElementById('yellow');
+
+const reqServer = (color) => {
+    fetch('http://localhost:8080/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ color })
+    })
+        .then(resData => {
+            return resData.json()
+        })
+        .then(data => {
+            const { color } = data;
+            document.body.style.backgroundColor = color;
+        })
+        .catch(err => {
+            console.log('err', err)
+        })
+}
+
+red.addEventListener('click', (e) => reqServer('red'))
+yellow.addEventListener('click', (e) => reqServer('yellow'))
+
+fetch('http://localhost:8080/')
+    .then(resData => {
+        return resData.json()
+    })
+    .then(data => {
+        const { color } = data;
+        document.body.style.backgroundColor = color;
+    })
+    .catch(err => {
+        console.log('err', err)
+    })
